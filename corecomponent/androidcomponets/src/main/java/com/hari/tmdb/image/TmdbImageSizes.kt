f15944181e409a525469b66b4a0a404124c79fdd
Hari Choudhary <harikulhari06@gmail.com>
2020-04-23 18:41:08 +0530

    feature movie details

---
+++ corecomponent/androidcomponets/src/main/java/com/hari/tmdb/image/TmdbImageSizes.kt
@@ -0,0 +1,33 @@
+package com.hari.tmdb.image
+
+object TmdbImageSizes {
+
+    const val baseImageUrl = "https://image.tmdb.org/t/p/"
+
+    val posterSizes = listOf(
+        "w92",
+        "w154",
+        "w185",
+        "w342",
+        "w500",
+        "w780",
+        "original"
+    )
+
+    val backdropSizes = listOf(
+        "w300",
+        "w780",
+        "w1280",
+        "original"
+    )
+
+    val logoSizes = listOf(
+        "w45",
+        "w92",
+        "w154",
+        "w185",
+        "w300",
+        "w500",
+        "original"
+    )
+}
