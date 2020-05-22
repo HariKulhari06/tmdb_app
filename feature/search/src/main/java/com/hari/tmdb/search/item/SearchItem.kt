be9a08c3c14b8cf9c551b0ee8c7d7d43e06e14e7
Hari Choudhary <harikulhari06@gmail.com>
2020-05-22 16:41:43 +0530

    click effect

--- feature/search/src/main/java/com/hari/tmdb/search/item/SearchItem.kt
+++ feature/search/src/main/java/com/hari/tmdb/search/item/SearchItem.kt
@@ -2,12 +2,9 @@ package com.hari.tmdb.search.item
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.hari.tmdb.model.Movie
 import com.hari.tmdb.search.R
 import com.hari.tmdb.search.databinding.ItemSearchBinding
-import com.hari.tmdb.search.ui.SearchFragmentDirections.Companion.actionSearchToMovieDetail
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import com.xwray.groupie.databinding.BindableItem
@@ -22,13 +19,13 @@ class SearchItem @AssistedInject constructor(
     override fun bind(viewBinding: ItemSearchBinding, position: Int) {
         viewBinding.movie = movie
         viewBinding.imageViewPoster.setOnClickListener {
-            val extra = FragmentNavigatorExtras(
+          /*  val extra = FragmentNavigatorExtras(
                 viewBinding.imageViewPoster to viewBinding.imageViewPoster.transitionName
             )
             viewBinding.imageViewPoster.findNavController().navigate(
                 actionSearchToMovieDetail(movieId = movie.id, title = movie.title),
                 extra
-            )
+            )*/
         }
     }
 
